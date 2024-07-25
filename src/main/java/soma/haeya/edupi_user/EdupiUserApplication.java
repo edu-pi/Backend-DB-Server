5f1092e8d1a64a2f643bc851b57b5295f6aeeaee
GS_song98 <20003204@sju.ac.kr>
2024-07-25 16:50:08 +0900

    initial commit

---
+++ src/main/java/soma/haeya/edupi_user/EdupiUserApplication.java
@@ -0,0 +1,13 @@
+package soma.haeya.edupi_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdupiUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdupiUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/soma/haeya/edupi_user/EdupiUserApplicationTests.java
@@ -0,0 +1,13 @@
+package soma.haeya.edupi_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdupiUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
