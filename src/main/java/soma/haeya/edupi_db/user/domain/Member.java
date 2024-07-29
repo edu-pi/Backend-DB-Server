3d9bc8c4645d0d901a91282561301c89d661e6d6
UJeans <jean207@naver.com>
2024-07-29 14:22:50 +0900

    [#1]feat: Member Entity 컬럼 추가(is_enabled, createdAt, pwdUpdatedAt, lastAccessAt)

--- src/main/java/soma/haeya/edupi_db/user/domain/Member.java
+++ src/main/java/soma/haeya/edupi_db/user/domain/Member.java
@@ -1,25 +1,44 @@
 package soma.haeya.edupi_db.user.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter @Setter
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "member_id")
+    @Column(name = "member_id", updatable = false)
     private Long id;
 
-    @Column(nullable = false)
-    private String name;
-
-    @Column(nullable = false, unique = true)
+    @NotNull
+    @Column(unique = true, updatable = false)
     private String email;
 
+    @NotNull
     @Column(nullable = false)
     private String password;
 
+    @NotNull
+    @Column(nullable = false)
+    private String name;
+
+    @NotNull
+    @Column(nullable = false)
+    private Boolean is_enabled;
+
     private String phoneNumber;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime pwdUpdatedAt;
+
+    private LocalDateTime lastAccessAt;
 }
