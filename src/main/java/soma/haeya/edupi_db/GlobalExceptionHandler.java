cb6e41abe1cc901c73d56c0b02386290e85083a3
UJeans <jean207@naver.com>
2024-07-29 16:06:10 +0900

    [#1]feat: 전역 에러 핸들러

---
+++ src/main/java/soma/haeya/edupi_db/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package soma.haeya.edupi_db;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import soma.haeya.edupi_db.user.domain.response.ErrorResponse;
+import soma.haeya.edupi_db.user.exception.UserFriendlyException;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(UserFriendlyException.class)
+    public ResponseEntity<ErrorResponse> handleUserFriendlyException(UserFriendlyException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST);
+        log.error("[GlobalExceptionHandler] UserFriendlyException 예외 발생 : {}", ex.getMessage());
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(errorResponse);
+    }
+}
