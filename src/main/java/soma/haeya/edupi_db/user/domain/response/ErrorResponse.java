16e1108ea06f538f74fdafa193d931a7c124ce9a
UJeans <jean207@naver.com>
2024-07-29 17:12:42 +0900

    [#1]fix: response 객체 record 클래스로 변경

--- src/main/java/soma/haeya/edupi_db/user/domain/response/ErrorResponse.java
+++ src/main/java/soma/haeya/edupi_db/user/domain/response/ErrorResponse.java
@@ -1,18 +1,6 @@
 package soma.haeya.edupi_db.user.domain.response;
 
-import lombok.Data;
-import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
-@Getter
-public class ErrorResponse {
-
-    private final String message;
-    private final HttpStatus status;
-
-    public ErrorResponse(String message, HttpStatus status) {
-        this.message = message;
-        this.status = status;
-    }
-
+public record ErrorResponse(String message, HttpStatus status) {
 }
--- src/main/java/soma/haeya/edupi_db/user/domain/response/SuccessResponse.java
+++ src/main/java/soma/haeya/edupi_db/user/domain/response/SuccessResponse.java
@@ -1,12 +1,4 @@
 package soma.haeya.edupi_db.user.domain.response;
 
-import lombok.Getter;
-
-@Getter
-public class SuccessResponse {
-    private final String message;
-
-    public SuccessResponse(String message) {
-        this.message = message;
-    }
+public record SuccessResponse(String message) {
 }
