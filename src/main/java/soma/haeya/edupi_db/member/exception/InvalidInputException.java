b5a414d3df330ed585d58310372897c47b8437fe
UJeans <jean207@naver.com>
2024-09-03 20:29:18 +0900

    [#15]feat: ServerException 추가

--- src/main/java/soma/haeya/edupi_db/member/exception/InvalidInputException.java
+++ src/main/java/soma/haeya/edupi_db/member/exception/InvalidInputException.java
@@ -3,12 +3,10 @@ package soma.haeya.edupi_db.member.exception;
 import lombok.Getter;
 
 /**
- * 사용자에게 에러의 원인을 보여줘야 할 때 사용하는 Exception
+ * 사용자의 유효하지 않는 입력에 대한 Exception
  */
 @Getter
 public class InvalidInputException extends RuntimeException {
-
-    private String errorCode;
 
     public InvalidInputException(String message) {
         super(message);
