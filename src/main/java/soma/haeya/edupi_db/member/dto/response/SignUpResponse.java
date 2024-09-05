3743936bd2155936c4f72367d9684c47a656a56d
UJeans <jean207@naver.com>
2024-09-05 16:40:12 +0900

    [#15]rename: SignUpResponse -> SignupResponse

--- src/main/java/soma/haeya/edupi_db/member/dto/response/SignUpResponse.java
+++ src/main/java/soma/haeya/edupi_db/member/dto/response/SignUpResponse.java
@@ -1,15 +1,9 @@
 package soma.haeya.edupi_db.member.dto.response;
 
-import lombok.Getter;
-
-@Getter
-public class SignUpResponse {
-
+public class SignupResponse {
     String message;
-    Long memberId;
 
-    public SignUpResponse(Long memberId) {
+    public SignupResponse() {
         this.message = "회원가입이 완료되었습니다.";
-        this.memberId = memberId;
     }
 }
