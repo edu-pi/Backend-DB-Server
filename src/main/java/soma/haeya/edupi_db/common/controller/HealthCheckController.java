9d4b392f4d71828bcfc5979d8094d2c8a416da44
GS_song98 <20003204@sju.ac.kr>
2024-09-08 22:56:02 +0900

    [#19]rename: HealthCheckController 위치 common으로 이동

--- src/main/java/soma/haeya/edupi_db/common/controller/HealthCheckController.java
+++ src/main/java/soma/haeya/edupi_db/common/controller/HealthCheckController.java
@@ -1,4 +1,4 @@
-package soma.haeya.edupi_db.member.controller;
+package soma.haeya.edupi_db.common.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
