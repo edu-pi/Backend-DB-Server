2049dbb55ac1ada541fd1e363096686149ace772
GS_song98 <20003204@sju.ac.kr>
2024-09-09 16:13:24 +0900

    [#19]feat: Valid 예외 처리 추가

--- src/main/java/soma/haeya/edupi_db/classroom/exception/ClassroomExceptionHandler.java
+++ src/main/java/soma/haeya/edupi_db/classroom/exception/ClassroomExceptionHandler.java
@@ -1,6 +1,8 @@
 package soma.haeya.edupi_db.classroom.exception;
 
+import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,7 +12,7 @@ import soma.haeya.edupi_db.common.exception.ExceptionLogger;
 import soma.haeya.edupi_db.common.model.response.DefaultErrorResponse;
 
 @Slf4j
-@RestControllerAdvice
+@RestControllerAdvice(basePackages = {"soma.haeya.edupi_db.classroom"})
 public class ClassroomExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(DbServerException.class)
@@ -20,6 +22,16 @@ public class ClassroomExceptionHandler extends ResponseEntityExceptionHandler {
         return ResponseEntity
             .status(exception.getHttpStatus())
             .body(new DefaultErrorResponse(exception.getMessage()));
+    }
+
+    // Valid 에러 핸들링
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<DefaultErrorResponse> validationError(ConstraintViolationException exception) {
+        ExceptionLogger.printErrorLog(exception);
+
+        return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(new DefaultErrorResponse("잘못된 요청입니다."));
     }
 
 
