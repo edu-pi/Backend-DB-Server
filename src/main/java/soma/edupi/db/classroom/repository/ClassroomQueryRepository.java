daa9d7b91b4a862ce5b8a357762d98e2b29e717a
GS_song98 <20003204@sju.ac.kr>
2024-09-12 21:55:47 +0900

    [#28]feat: 안쓰는 메서드, 접근자 삭제

--- src/main/java/soma/edupi/db/classroom/repository/ClassroomQueryRepository.java
+++ src/main/java/soma/edupi/db/classroom/repository/ClassroomQueryRepository.java
@@ -5,5 +5,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ClassroomQueryRepository {
 
-    public boolean existsLeaderClassroomByAccountIdAndName(Long accountId, String name);
+    boolean existsLeaderClassroomByAccountIdAndName(Long accountId, String name);
 }
--- src/main/java/soma/edupi/db/classroomAccount/repository/ClassroomAccountRepository.java
+++ src/main/java/soma/edupi/db/classroomAccount/repository/ClassroomAccountRepository.java
@@ -1,14 +1,10 @@
 package soma.edupi.db.classroomAccount.repository;
 
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import soma.edupi.db.classroomAccount.domain.ClassroomAccount;
-import soma.edupi.db.classroomAccount.domain.ClassroomAccountRole;
 
 @Repository
 public interface ClassroomAccountRepository extends JpaRepository<ClassroomAccount, Long> {
-
-    Optional<ClassroomAccount> findByAccountIdAndRole(Long classroomId, ClassroomAccountRole role);
 
 }
