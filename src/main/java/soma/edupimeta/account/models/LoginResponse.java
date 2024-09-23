94db0975e3ded2bfd7893a4c756304def422b795
GS_song98 <20003204@sju.ac.kr>
2024-09-23 15:30:01 +0900

    [#34]fix: record -> class

--- src/main/java/soma/edupimeta/account/models/LoginResponse.java
+++ src/main/java/soma/edupimeta/account/models/LoginResponse.java
@@ -1,10 +1,24 @@
 package soma.edupimeta.account.models;
 
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import soma.edupimeta.account.service.domain.Account;
 
-@Builder
-public record LoginResponse(String email, String name, String role) {
+@Getter
+@NoArgsConstructor
+public class LoginResponse {
+
+    private String email;
+    private String name;
+    private String role;
+
+    @Builder
+    public LoginResponse(String email, String name, String role) {
+        this.email = email;
+        this.name = name;
+        this.role = role;
+    }
 
     public static LoginResponse of(Account member) {
         return LoginResponse.builder()
