7368ee6dd92c134a524228c5fc4f19eaffe0d355
GS_song98 <20003204@sju.ac.kr>
2024-09-24 14:43:41 +0900

    [#]fix: login 시에 id값을 반환하도록 수정

--- src/main/java/soma/edupimeta/account/models/LoginResponse.java
+++ src/main/java/soma/edupimeta/account/models/LoginResponse.java
@@ -9,22 +9,25 @@ import soma.edupimeta.account.service.domain.Account;
 @NoArgsConstructor
 public class LoginResponse {
 
+    private Long id;
     private String email;
     private String name;
     private String role;
 
     @Builder
-    public LoginResponse(String email, String name, String role) {
+    public LoginResponse(Long id, String email, String name, String role) {
+        this.id = id;
         this.email = email;
         this.name = name;
         this.role = role;
     }
 
-    public static LoginResponse of(Account member) {
+    public static LoginResponse of(Account account) {
         return LoginResponse.builder()
-            .email(member.getEmail())
-            .name(member.getName())
-            .role(member.getRole())
+            .id(account.getId())
+            .email(account.getEmail())
+            .name(account.getName())
+            .role(account.getRole())
             .build();
     }
 }
