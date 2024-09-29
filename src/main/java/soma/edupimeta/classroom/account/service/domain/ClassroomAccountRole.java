d6a441b4aa7f4cd70516c0b74e48144f13c350d0
GS_song98 <20003204@sju.ac.kr>
2024-09-29 13:26:20 +0900

    [#40]fix: ClassroomAccountRole 문자 대신 숫자로 통일

--- src/main/java/soma/edupimeta/classroom/account/service/domain/ClassroomAccountRole.java
+++ src/main/java/soma/edupimeta/classroom/account/service/domain/ClassroomAccountRole.java
@@ -1,6 +1,29 @@
 package soma.edupimeta.classroom.account.service.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum ClassroomAccountRole {
-    HOST,
-    GUEST
+    HOST(1), GUEST(2);
+
+    private final int value;
+
+    ClassroomAccountRole(int value) {
+        this.value = value;
+    }
+
+    @JsonCreator
+    public static ClassroomAccountRole fromValue(int value) {
+        for (ClassroomAccountRole role : ClassroomAccountRole.values()) {
+            if (role.getValue() == value) {
+                return role;
+            }
+        }
+        throw new IllegalArgumentException("Invalid value for ClassroomAccountRole: " + value);
+    }
+
+    @JsonValue
+    public int getValue() {
+        return value;
+    }
 }
