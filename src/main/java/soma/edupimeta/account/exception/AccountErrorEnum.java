2874564dfb9982090a8399afc85bdc9b96a709cb
UJeans <jean207@naver.com>
2024-11-06 15:41:11 +0900

    [#62]feat: 로그인 시 활성화 여부 체크

--- src/main/java/soma/edupimeta/account/exception/AccountErrorEnum.java
+++ src/main/java/soma/edupimeta/account/exception/AccountErrorEnum.java
@@ -8,6 +8,7 @@ AccountErrorEnum implements ErrorEnum {
     // 400
     EMAIL_NOT_MATCH(HttpStatus.BAD_REQUEST, "DB-400001", "존재하지 않는 회원입니다."),
     EMAIL_OR_PASSWORD_NOT_MATCH(HttpStatus.BAD_REQUEST, "DB-400002", "아이디 혹은 비밀번호가 일치하지 않습니다."),
+    NOT_ACTIVATED_EXCEPTION(HttpStatus.BAD_REQUEST, "DB-400003", "이메일 인증 되지 않은 사용자입니다."),
 
     // 409
     EMAIL_DUPLICATE(HttpStatus.CONFLICT, "DB-409001", "이메일 중복입니다."),
